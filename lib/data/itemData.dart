0ede8a6d295306b09cc0e3e6473398fb1667ea9d
ThairongSeng <seng.thairong@gmail.com>
2023-10-04 21:57:51 +0700

    this is the expansion panel in flutter

---
+++ lib/data/itemData.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/material.dart';
+
+import '../model/ItemModel.dart';
+
+List<ItemModel> itemData = <ItemModel>[
+  ItemModel(
+      headerItem: 'Android',
+      description:
+      "Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets. ... Some well known derivatives include Android TV for televisions and Wear OS for wearables, both developed by Google.",
+      colorsItem: Colors.green,
+      img: 'assets/images/android.png'
+  ),
+  ItemModel(
+      headerItem: 'Android',
+      description:
+      "Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets. ... Some well known derivatives include Android TV for televisions and Wear OS for wearables, both developed by Google.",
+      colorsItem: Colors.green,
+      img: 'assets/images/android.png'
+  ),
+  ItemModel(
+      headerItem: 'Android',
+      description:
+      "Android is a mobile operating system based on a modified version of the Linux kernel and other open source software, designed primarily for touchscreen mobile devices such as smartphones and tablets. ... Some well known derivatives include Android TV for televisions and Wear OS for wearables, both developed by Google.",
+      colorsItem: Colors.green,
+      img: 'assets/images/android.png'
+  ),
+
+];
---
+++ lib/view/MyHomePage.dart
@@ -0,0 +1,86 @@
+
+import 'package:flutter/material.dart';
+import '../../data/itemData.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expansion Panel Demo'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: ListView.builder(
+          itemCount: itemData.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ExpansionPanelList(
+              animationDuration: const Duration(milliseconds: 800),
+              dividerColor: Colors.red,
+              elevation: 1,
+              children: [
+                ExpansionPanel(
+                  body: Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        ClipOval(
+                          child: CircleAvatar(
+                            child: Image.asset(
+                              itemData[index].img,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Text(
+                          itemData[index].description,
+                          style: TextStyle(
+                              color: Colors.grey[700],
+                              fontSize: 15,
+                              letterSpacing: 0.3,
+                              height: 1.3),
+                        ),
+                      ],
+                    ),
+                  ),
+                  headerBuilder: (BuildContext context, bool isExpanded) {
+                    return Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        itemData[index].headerItem,
+                        style: TextStyle(
+                          color: itemData[index].colorsItem,
+                          fontSize: 18,
+                        ),
+                      ),
+                    );
+                  },
+                  isExpanded: itemData[index].expanded,
+                )
+              ],
+              expansionCallback: (int item, bool status) {
+                setState(() {
+                  itemData[index].expanded = !itemData[index].expanded;
+                });
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+
+}
